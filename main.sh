be0a8450d1bd3d859c7fa746447bc950ffd20749
Kevin <kevinsalvador2008@outlook.com>
2022-08-01 15:27:25 -0500

    fix

--- main.sh
+++ main.sh
@@ -3,11 +3,6 @@
 fix=0
 
 
-# Stuff to do at first run(submodule, npm install).
-if [[ -d "static" && -n "$(find static -prune -empty 2>/dev/null)" ]] || [[ ! -d "static" ]]; then
-  npm install
-  git submodule update --init --recursive
-fi
 
 
-npm start || [[ $fix = 1 ]] && npm install && npm update && git submodule update --init --recursive
+npm start 
