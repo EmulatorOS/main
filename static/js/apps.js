40ad6be21013f52fcc1c87d184728e04b496b27a
Kevin <kevinsalvador2008@outlook.com>
2022-08-03 20:44:39 -0500

    set apps

--- static/js/apps.js
+++ static/js/apps.js
@@ -86,3 +86,61 @@ function searchapps() {
   }
   
   fetchapps()
+  var surf = document.getElementById('appframe')
+  function setTabTitle(title) {
+    if (title) {
+      var navtitle = document.getElementById("nav-title");
+      navtitle.innerText = title;
+      document.title = title;
+    } else {
+      var navtitle = document.getElementById("nav-title");
+      var surf = document.getElementById("appframe");
+      navtitle.innerText = surf.contentWindow.location.host;
+    }
+  }
+  
+  function setTabIcon(favicon) {
+    if (favicon) {
+      var navicon = document.getElementById("nav-icon");
+      navicon.src = favicon;
+    } else {
+      var navicon = document.getElementById("nav-icon");
+      navicon.src =
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEyIDJjNS41MiAwIDEwIDQuNDggMTAgMTBzLTQuNDggMTAtMTAgMTBTMiAxNy41MiAyIDEyIDYuNDggMiAxMiAyek00IDEyaDQuNGMzLjQwNy4wMjIgNC45MjIgMS43MyA0LjU0MyA1LjEyN0g5LjQ4OHYyLjQ3YTguMDA0IDguMDA0IDAgMDAxMC40OTgtOC4wODNDMTkuMzI3IDEyLjUwNCAxOC4zMzIgMTMgMTcgMTNjLTIuMTM3IDAtMy4yMDYtLjkxNi0zLjIwNi0yLjc1aC0zLjc0OGMtLjI3NC0yLjcyOC42ODMtNC4wOTIgMi44Ny00LjA5MiAwLS45NzUuMzI3LTEuNTk3LjgxMS0xLjk3QTguMDA0IDguMDA0IDAgMDA0IDEyeiIgZmlsbD0iIzNDNDA0MyIvPjwvc3ZnPg==";
+    }
+  }
+  surf.addEventListener("load", function () {
+    var navtitle = document.getElementById("nav-title");
+    var navicon = document.getElementById("nav-icon");
+  
+    if (surf.contentWindow.location.toString() == "about:blank") {
+      return {
+        name: (navtitle.innerText = "Loading..."),
+        favicon: (navicon.src = ""),
+      };
+    }
+  
+    var initTitle = surf.contentWindow.document.title;
+    setTabTitle(initTitle);
+    console.log(initTitle);
+  
+    var initFavicon = null;
+    var icon =
+      surf.contentWindow.document.querySelector("link[rel='icon']") || null;
+    var shortcuticon =
+      surf.contentWindow.document.querySelector("link[rel='shortcut icon']") ||
+      null;
+    if (icon) {
+      initFavicon = new URL(surf.contentWindow.document.querySelector("link[rel='icon']").getAttribute("href"), surf.contentWindow.document.baseURI).toString();
+      document.querySelector("link[rel=icon]").href = initFavicon;
+      console.log('icon is ' + initFavicon)
+    } else if (shortcuticon) {
+      initFavicon = new URL(surf.contentWindow.document.querySelector("link[rel='shortcut icon']").getAttribute("href"), surf.contentWindow.document.baseURI).toString();
+      document.querySelector("link[rel=shortcut icon]").href = initFavicon;
+    }
+    if (initFavicon == surf.contentWindow.document.baseURI) {
+      initFavicon = null;
+      console.log('favicon is null')
+    }
+    setTabIcon(initFavicon);
+  });
