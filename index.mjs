c81122eaa14810abd597efdcdab095f90890fc84
KSalvador 06757 <102191619+EmulatorOS@users.noreply.github.com>
2022-11-05 20:13:54 +0000

    Fix

--- index.mjs
+++ index.mjs
@@ -1,75 +1,29 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const express = require("express")
-const app = express()
-const config = require("./config.json")
-const port = process.env.PORT || config.port
-const Corrosion = require("./lib/server")
-import RhodiumProxy from 'Rhodium';
-import fetch from 'node-fetch';
-
-import Server from 'bare-server-node';
+import createServer from '@tomphttp/bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
 
-const bare =  new Server('/bare/', '');
+const bare =  createServer('/bare/');
+const serve = new nodeStatic.Server('static/');
 
-const proxy = new Corrosion({
-    prefix: "/corrosion/",
-    codec: "xor",
-    title: "Tsunami",
-    forceHttps: true,
-    requestMiddleware: [
-        Corrosion.middleware.blacklist([
-            "accounts.google.com",
-        ], "Page is blocked"),
-    ]
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+		serve.serve(req, res);
+	}
 });
 
-proxy.bundleScripts();
-
-const Rhodium = new RhodiumProxy({
-  encode: "xor",
-  prefix: "/rhodium/",
-  server: app,
-  Corrosion: [true, proxy],
-  title: "Tsunami"
-})
-
-Rhodium.init();
-
-app.use(express.static("./static", {
-    extensions: ["html"]
-}));
-
-app.get("/", function(req, res){
-    res.sendFile("index.html", {root: "./static"});
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	}else{
+		socket.end();
+	}
 });
 
-app.get("/suggestions", function(req, res){
-async function getsuggestions() {
-var term = req.query.q || "";
-var response = await fetch("https://duckduckgo.com/ac/?q=" + term + "&type=list");
-var result = await response.json();
-var suggestions = result[1]
-res.send(suggestions)
-}
-getsuggestions()
+server.listen({
+	port: process.env.PORT || 8080,
 });
-app.use(function (req, res) {
-    if (req.url.startsWith(proxy.prefix)) {
-      proxy.request(req,res);
-    } else if (req.url.startsWith(Rhodium.prefix)) {
-      return Rhodium.request(req, res)
-    } else if (req.url.startsWith("/bare/")) {
-      return bare.route_request(req, res)
-    } else {
-      res.status(404).sendFile("404.html", {root: "./public"});
-    }
-})
-
-
-app.listen(port, () => {
-  console.log(`EmulatorOS is running at localhost:${port}`)
-})
