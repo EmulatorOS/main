a9397c4b60eb0f0d99a32bd6c3b111b5fab56500
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-29 23:04:56 -0600

    maybe?

--- index.mjs
+++ index.mjs
@@ -27,21 +27,6 @@ app.get("/", function(req, res){
 app.set('trust proxy', 1)
 
 // Enable cors
-const allowedOrigins = ['https://emulatoros.github.io',
-                      'https://emulatoros2.github.io'];
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.' + origin;
-       
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-
-}));
-// Set static folder
 app.use(express.static('public'))
 
 // Routes
