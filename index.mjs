39f050e6b6c4bd0a6879a78bc8f530bb4923b102
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-11-29 23:12:25 -0600

    *

--- index.mjs
+++ index.mjs
@@ -29,15 +29,7 @@ app.set('trust proxy', 1)
 // Enable cors
 const allowedOrigins = ['*'];
 app.use(cors({
-  origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.' + origin;
-       
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
+  origin: '*'
 
 }));
 // Set static folder
